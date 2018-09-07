f4fdaa801df49b4074a2967f78ebd434dfed674d
Jeremy Campbell <jcampbell2@harding.edu>
2018-09-06 19:03:28 -0500

    Program done and tested. Will now return whether or not 2 strings are anagrams.

--- Anagrams/anagrams.cpp
+++ Anagrams/anagrams.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <string>
 
 using std::cout;
 using std::cin;
@@ -8,6 +9,30 @@ using std::endl;
 using std::ifstream;
 using std::ofstream;
 using std::vector;
+using std::string;
+
+void countLetters(string line, vector<int> &counts)
+{
+	for (auto letter : line)
+	{
+		if (isalpha(letter))
+		{
+			char upper = toupper(letter);
+			counts[upper - 'A']++;
+		}
+	}
+}
+
+bool anagram(string line1, string line2)
+{
+	vector<int> counts1(26, 0);
+	vector<int> counts2(26, 0);
+
+	countLetters(line1, counts1);
+	countLetters(line2, counts2);
+
+	return (counts1 == counts2) ? true : false;
+}
 
 int main()
 {
@@ -19,7 +44,24 @@ int main()
 	}
 
 	ofstream fout("anagrams.out");
+	const string endOfFile = "<end>";
 
+	// Read in 2 lines
+	string line1, line2;
+	getline(fin, line1);
+	getline(fin, line2);
+	while (line1 != endOfFile && line2 != endOfFile)
+	{
+		bool result = anagram(line1, line2);
+		fout << ((result) ? "YES" : "NO");
 
+		getline(fin, line1);
+		getline(fin, line2);
+
+		if (line1 != endOfFile && line2 != endOfFile)
+		{
+			fout << endl;
+		}
+	}
 	return 0;
 }
